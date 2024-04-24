7184f104c0e91856f3d0a7683a527b995610ec1f
Alyssa Cooper <alyssajcooper3@gmail.com>
2024-04-24 09:43:06 -0500

    first commit

---
+++ StoryChoiceGame/StoryChoiceGame/StoryChoiceGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoryChoiceGameApp.swift
+//  StoryChoiceGame
+//
+//  Created by COOPER, ALYSSA J. on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct StoryChoiceGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
