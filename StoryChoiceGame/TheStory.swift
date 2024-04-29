7191fb47e7961b626ff66104a1b03db5b968d451
Alyssa Cooper <alyssajcooper3@gmail.com>
2024-04-29 10:18:39 -0500

    edited story

--- StoryChoiceGame/TheStory.swift
+++ StoryChoiceGame/TheStory.swift
@@ -12,22 +12,14 @@ let story = Story(pages: [
         """
         Welcome to the haunted OTC Hamra Library 📚👻.
 
-        You receive a mysterious invitation to a late-night book reading. The heavy oak doors creak open by themselves as you approach. The hall is dimly lit, filled with the musty scent of old books. Where do you go first?
+        You receive an invitation to a late-night study session. The heavy oak doors creak open by themselves as you approach. The halls are dimly lit, filled with the musty scent of old books. Where do you go first?
         """,
         choices: [
             Choice(text: "Explore the dusty old archives.", destination: 1),
-            Choice(text: "Head straight to the reading room.", destination: 2),
-            Choice(text: "Check out the forbidden section.", destination: 3),
-        ]
-    ),
-    StoryPage( // Page 1
-        """
-        You wander into the archives. The air is thick with dust. As you browse through ancient texts, a hidden switch on the bookshelf reveals a secret passage. The passage emits a cold, eerie draft. Do you dare enter?
-        """,
-        choices: [
-            Choice(text: "Enter the secret passage.", destination: 4),
-            Choice(text: "Return to the main hall.", destination: 0),
-        ]
+            Choice(text: "Head straight to the quiet room.", destination: 2),
+            Choice(text: "Check out the forgotten section.", destination: 3),
+        ],
+        imageName: "image"
     ),
     StoryPage( // Page 2
         """
@@ -40,47 +32,98 @@ let story = Story(pages: [
     ),
     StoryPage( // Page 3
         """
-        The forbidden section is ominously quiet, the books untouched by time. You find a book with your name on the cover, glowing faintly in the shadows. Do you open it?
+        The forgotten section is ominously quiet, the books untouched by time. You find a book with your name on the cover. Do you open it?
         """,
         choices: [
             Choice(text: "Open the book.", destination: 6),
             Choice(text: "This feels wrong. Leave immediately.", destination: 0),
         ]
     ),
-    StoryPage( // Page 4
-        """
-        The passage leads to an underground chamber, its walls lined with strange artifacts and ancient scripts. A lone pedestal in the center holds a mysterious, pulsating orb. Do you touch the orb?
-        """,
-        choices: [
-            Choice(text: "Touch the orb.", destination: 7),
-            Choice(text: "No, go back to the archives.", destination: 1),
-        ]
-    ),
-    StoryPage( // Page 5
-        """
-        As the figure continues reading, the air grows colder, and the other guests begin to disappear one by one. You feel an unseen force binding you to your chair. Do you fight to break free, or succumb to the eerie tale?
-        """,
-        choices: [
-            Choice(text: "Fight the unseen force.", destination: 8),
-            Choice(text: "Do nothing and listen.", destination: 9),
-        ]
-    ),
     StoryPage( // Page 6
         """
-        As you open the book, spectral figures emerge from the pages, their whispers filling the air. They speak of ancient debts and curses. One ghost extends its hand, offering you a deal. Do you accept?
+        As you open the book, you hear whispers. You think you hear your name... What do you do?
         """,
         choices: [
-            Choice(text: "Accept the ghost's offer.", destination: 10),
-            Choice(text: "Refuse and close the book.", destination: 3),
+            Choice(text: "Investigate the noise.", destination: 10),
+            Choice(text: "Keep reading the book.", destination: 3),
         ]
     ),
-    StoryPage( // Page 7
-        """
-        As soon as you touch the orb, visions of the library's grim past flash before your eyes. The founding librarian was a warlock who cursed the building. You now hold the power to lift the curse. What do you do?
-        """,
-        choices: [
-            Choice(text: "Lift the curse.", destination: 11),
-            Choice(text: "Leave the curse in place.", destination: 12),
-        ]
-    )
+    StoryPage( // Page 8
+            """
+            Struggling against the invisible force, you manage to break free. The hooded figure notices and vanishes into thin air, leaving behind a key. Picking it up, you notice it's labeled 'Vault'. Where do you use it?
+            """,
+            choices: [
+                Choice(text: "Try it on a locked door in the main hall.", destination: 13),
+                Choice(text: "Go back to the forbidden section.", destination: 3),
+            ]
+        ),
+        StoryPage( // Page 9
+            """
+            You continue to listen, hypnotized by the tale. As the final words are spoken, reality shifts—you are no longer in the library, but in a ghostly realm. A spectral librarian asks if you wish to return or stay. What is your choice?
+            """,
+            choices: [
+                Choice(text: "Ask to return to reality.", destination: 14),
+                Choice(text: "Choose to stay in the ghostly realm.", destination: 15),
+            ]
+        ),
+        StoryPage( // Page 10
+            """
+            You turn around and walk down the aisles of books. You freeze in place when you see a pool of blood in the floor. You decide to...
+            """,
+            choices: [
+                Choice(text: "page 4", destination: 4),
+                Choice(text: "Pull out your phone", destination: 16),
+            ]
+        ),
+        StoryPage( // Page 11
+            """
+            You continue running until you get to a quiet room at the back of the library. You slam the door and lock it behind you. You grab a chair and shove it under the door handle. The figure bangs on the door. What do you do?
+            """,
+            choices: [
+                Choice(text: "You look around and see a vent on the ceiling", destination: 17),
+                Choice(text: "You grab a notebook from the table and hold it up.", destination: 19),
+            ]
+        ),
+        StoryPage( // Page 15
+            """
+            You keep crawling until you reach another vent. You kick through the vent and drop down into the hall outside the library. You take off sprinting and bust through the door to get outside...
+            """,
+            choices: []
+        ),
+        StoryPage( // Page 16
+            """
+            You open your phone to call the police, but you have no service! You run back to the door and find it locked. You turn around and standing there is a tall figure in all black with a sharp, gleaming axe pointed at your face. What do you do?
+            """,
+            choices: [
+                Choice(text: "You duck and run!", destination: 11),
+                Choice(text: "You hesitate and the axe comes flying at you.", destination: 18),
+            ]
+        ),
+        StoryPage( // Page 17
+            """
+            You quickly climb up on a chair, with the figure still banging at the door. You hoist yourself up through the vent and start crawling. You come to an intersection. Left or right?
+            """,
+            choices: [
+                Choice(text: "Left", destination: 15),
+                Choice(text: "Right", destination: 20),
+            ]
+        ),
+        StoryPage( // Page 18
+            """
+            You die a gruesome death. Game overrr!
+            """,
+            choices: []
+        ),
+        StoryPage( // Page 19
+            """
+            The figure burst through the door with the axe. You jump back and swing your notebook. The figure deflects it and slashes at you. Everything goes black as you feel the axe slice through you.
+            """,
+            choices: []
+        ),
+        StoryPage( // Page 20
+            """
+            You start crawling as fast as you can, but you fall through the vent and come crashing down onto a table. You wake up sometime later with the figure looming over you. The figure swngs the axe and it cuts right through you.
+            """,
+            choices: []
+        ),
 ])
